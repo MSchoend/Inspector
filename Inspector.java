e1f0d122a788979161cfc3539968522289ebabdd
Mark Schoendorfer <mark.schoendorfer@ucalgary.ca>
2019-10-23 22:42:56 -0600

    constructor printout

--- Inspector.java
+++ Inspector.java
@@ -1,5 +1,8 @@
 
 import java.lang.reflect.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 
 public class Inspector {
 
@@ -8,23 +11,48 @@ public class Inspector {
         inspectClass(c, obj, recursive, 0);
     }
 
-    private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
+    private void inspectClass(Class clazz, Object obj, boolean recursive, int depth) {
     	
     	//Output:
+    	String padding = "";
+    	for(int i = 0; i < depth; i++) {
+    		padding += "\t";
+    	}
+    	StringBuilder outputBuilder = new StringBuilder();
+    	
     	//Class Name
+    	outputBuilder.append(clazz.getName() + ":\n");
     	//Superclass (Recurse into superclass)
     	//Interfaces (Recurse)
     	//Constructors
+    	Constructor cons[] = clazz.getConstructors();
+    	for(Constructor c : cons) {
+    		outputBuilder.append(padding + " CONSTRUCTOR - ");
+    		outputBuilder.append(interpretModifiers(c.getModifiers()));
+    		outputBuilder.append(c.getName() + "(");
+    		
+    		Parameter params[] = c.getParameters();
+    		for(Parameter p : params) {
+    			if(p.getType().getComponentType() == null) {
+    				outputBuilder.append(p.getType() + " " + p.getName() + ", ");
+    			} else {
+    				outputBuilder.append(p.getType().getComponentType() + "[] " +p.getName() + ", ");
+    			}
+    		}
+    		outputBuilder.append(")\n");
+    	}
     		//name
     		//parameters + types
     		//modifiers
     	//Methods
+    	//Method methods[] = clazz.getMethods();
     		//name
     		//Exceptions
     		//parameters + types
     		//return type
     		//modifiers
     	//Fields
+    	//Field fields[] = clazz.getFields();
     		//name
     		//type
     		//modifiers
@@ -32,6 +60,44 @@ public class Inspector {
     		//If recursive set to true, recurse into non-primitive fields
     		//If recursive false, print out class + identity hash code
     	
+    	System.out.println(outputBuilder.toString());
+    }
+    
+    private String interpretModifiers(int mods) {
+    	String toReturn = " ";
+    	
+    	if(Modifier.isPublic(mods))
+    		toReturn += "public ";
+    	else if(Modifier.isPrivate(mods))
+    		toReturn += "private ";
+    	else if(Modifier.isProtected(mods))
+    		toReturn += "protected ";
+    	
+    	if(Modifier.isAbstract(mods))
+    		toReturn += "abstract ";
+    	
+    	if(Modifier.isStatic(mods))
+    		toReturn += "static ";
+    	
+    	if(Modifier.isFinal(mods))
+    		toReturn += "final ";
+    	
+    	if(Modifier.isTransient(mods))
+    		toReturn += "transient ";
+    	
+    	if(Modifier.isVolatile(mods))
+    		toReturn += "volatile ";
+    	
+    	if(Modifier.isSynchronized(mods))
+    		toReturn += "synchronized ";
+    	
+    	if(Modifier.isNative(mods))
+    		toReturn += "native ";
+    	
+    	if(Modifier.isStrict(mods))
+    		toReturn += "strict ";
+    	
+    	return toReturn;
     }
 
 }
